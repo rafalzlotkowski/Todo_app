e569e630662aa64bd2bfcfb7faa919726c656d34
Rafal <rafalzlotkowski136@gmail.com>
2026-04-08 11:45:53 +0200

    feat: add  todo inputs component

---
+++ src/app/components/todo-input/todo-input.ts
@@ -0,0 +1,24 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-todo-input',
+  imports: [],
+  templateUrl: './todo-input.html',
+  styleUrl: './todo-input.css',
+})
+export class TodoInput {
+  title: string = '';
+  description: string = '';
+
+  @Output() add = new EventEmitter<{ title: string; description: string }>();
+
+  onAdd() {
+    if (this.title.trim()) {
+      this.add.emit({ title: this.title, description: this.description });
+      this.title = '';
+      this.description = '';
+    }
+  }
+}
