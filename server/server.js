eadcc5260964d020aca6a41d723d17fcde966684
Rafal <rafalzlotkowski136@gmail.com>
2026-04-29 10:21:46 +0200

    add server

--- server/server.js
+++ server/server.js
@@ -5,30 +5,63 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-let nextId = 1;
 
-function writeData(data){
+function readData(data){
+    const data = fs.readFileSync('db.json');
+    return JSON.parse(data);
+}
+
+function readData(data){
     fs.writeFileSync('db.json',
-        JSON.stringify
-    )
+        JSON.stringify(data, null, 2)
+    );
 }
 app.get('/todos',(req , res) => {
-    res.json(todos)
+    const data = readData();
+    res.json(data, todos);
 })
 
 app.post('/todos',(req , res) =>{
+    const data = readData();
     const{title, description, dueDate} = req.body;
+    const nextId = data.todos.length > 0 ? data.todos[data.todos.length - 1].id + 1 : 1;
     const newTodo = {
-        id: nextId++,
+        id: nextId,
         title,
         description,
         dueDate,
         completed: false
     };
-    todos.push(newTodo);
+    data.todos.push(newTodo);
+    writeData(Data);
     res.json( newTodo);
 })
+app.put('/todos',(req , res) =>{
 
-server.listen(3000, () => {
+    const data = readData();
+    const id = Number(req.params.id);
+
+    const todo = data.todos.find(t => t.id === id);
+    if (!todo) {
+        return res.status(404).json({error:'Todo not found'});
+    }
+    Object.assign(todo, req.body);
+    writeData(data);
+    res.json(todo);
+    
+    
+});
+app.delete('/todos/id',(req, res)=>{
+    const data = readData();
+    const id = Number(req.params.id);
+    const index = data.todos.findIndex(t => t.id === id);
+    if (index === -1){
+        return res.status(404).json({error:'todo not found'})
+    }
+    writeData(data);
+    res.json({message:'todo deleted successfully'});
+})
+
+app.listen(3000, () => {
   console.log("JSON Server is running");
 });
